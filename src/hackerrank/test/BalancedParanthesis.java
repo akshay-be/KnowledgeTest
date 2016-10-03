eb6c870d7490bd2a8d41567c57c7e9d3112842f8
Dr. Arun <Dr. Arun>
2016-10-03 22:20:57 +0530

    old change

--- src/hackerrank/test/BalancedParanthesis.java
+++ src/hackerrank/test/BalancedParanthesis.java
@@ -1,5 +1,7 @@
 package hackerrank.test;
 
+import java.util.Stack;
+
 public class BalancedParanthesis {
 
 	public static void main(String[] args) {
@@ -7,4 +9,41 @@ public class BalancedParanthesis {
 		while(input.length() != (input = input.replaceAll("\\(\\)|\\[\\]|\\{\\}", "")).length());
 		    System.out.println(input.isEmpty());
 	}
+	
+	/**
+	 * Using stack implementation.
+	 * @param expression
+	 * @return
+	 */
+	public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0;i<expression.length();i++){
+            char ch = expression.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack.push(ch);
+            }else {
+                if(stack.size()==0){
+                    return false;
+                }else{
+                    
+                    char ch2 = stack.pop();
+                     if(ch==')'){
+                        if(ch2!='(') 
+                            return false;
+                     }else if(ch=='}'){
+                         if(ch2!='{') 
+                             return false;
+                     }else if(ch==']'){
+                        if(ch2!='[') 
+                            return false;
+                     }
+            }
+        
+        }
+        
+     }
+        if(stack.size()==0) 
+            return true;
+        else return false;
+    }
 }
--- src/interview/appmat/QuestionException.java
+++ src/interview/appmat/QuestionException.java
@@ -1,4 +1,4 @@
-package interview.am;
+package interview.appmat;
 
 public class QuestionException {
 
