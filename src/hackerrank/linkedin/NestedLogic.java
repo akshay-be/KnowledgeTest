cbfd9b60b7bcede370adb537e74c23fe6e8af891
t_akshayb1 <t_akshayb1@verifone.com>
2016-10-06 18:36:40 +0530

    todays.

---
+++ src/hackerrank/linkedin/NestedLogic.java
@@ -0,0 +1,28 @@
+package hackerrank.linkedin;
+
+public class NestedLogic {
+
+	/**
+	 * If the book is returned on or before the expected return date, no fine
+	 * will be charged (i.e.: fine = 0). 
+	 * 
+	 * If the book is returned after the
+	 * expected return day but still within the same calendar month and year as
+	 * the expected return date, fine = 15 Hackos * (the number of days late) .
+	 * 
+	 * If the book is returned after the expected return month but still within
+	 * the same calendar year as the expected return date, the fine = 500 Hackos
+	 * * (the number of months late) . 
+	 * 
+	 * If the book is returned after the
+	 * calendar year in which it was expected, there is a fixed fine of 10000.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String strReturnDate = "";
+		String strActualDate = "";
+
+	}
+
+}
